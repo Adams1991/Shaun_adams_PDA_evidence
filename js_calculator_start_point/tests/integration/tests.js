a353d7555709b6ccc1ebc4d429b73bdebd56463f
Adams1991 <adams121@hotmail.co.uk>
2018-08-24 15:33:25 +0100

    first integration test passing

--- js_calculator_start_point/tests/integration/tests.js
+++ js_calculator_start_point/tests/integration/tests.js
@@ -16,6 +16,13 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
   })
 
-  it('shu')
+  it('should the arithmetical operations update the display with the result of the operation', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_multiply')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('4')
+  })
 
 });
