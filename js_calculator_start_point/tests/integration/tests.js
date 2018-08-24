fb10179bfee6e1faa9dab21fe140af505dfbc161
Adams1991 <adams121@hotmail.co.uk>
2018-08-24 15:37:06 +0100

    3 integration tests passing

--- js_calculator_start_point/tests/integration/tests.js
+++ js_calculator_start_point/tests/integration/tests.js
@@ -16,13 +16,24 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('2')
   })
 
-  it('should the arithmetical operations update the display with the result of the operation', function(){
+  it('should update the display with the result of the operation after another operator is clicked', function(){
     running_total = element(by.css('#running_total'))
     element(by.css('#number2')).click();
     element(by.css('#operator_multiply')).click();
     element(by.css('#number2')).click();
     element(by.css('#operator_multiply')).click();
     expect(running_total.getAttribute('value')).to.eventually.equal('4')
+  })
+
+  it('should be able to chain together multiple operations', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_divide')).click();
+    element(by.css('#number4')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('1')
   })
 
 });
