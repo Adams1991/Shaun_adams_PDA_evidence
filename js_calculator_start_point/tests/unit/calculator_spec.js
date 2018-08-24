90c05c90e2c3f3eba8100c928241551c4cf49a1a
Adams1991 <adams121@hotmail.co.uk>
2018-08-24 11:38:57 +0100

    unit tests done

--- js_calculator_start_point/tests/unit/calculator_spec.js
+++ js_calculator_start_point/tests/unit/calculator_spec.js
@@ -11,4 +11,32 @@ describe('calculator', function () {
     assert.equal(true, true)
   })
 
+  it('it can add together two numbers', function(){
+    calculator.previousTotal = 1;
+    calculator.add(4);
+    const result = calculator.runningTotal
+    assert.equal(result, 5)
+  })
+
+  it('it can substract two numbers', function() {
+    calculator.previousTotal = 7;
+    calculator.subtract(4);
+    const result = calculator.runningTotal
+    assert.equal(result, 3)
+  })
+
+  it('it can multiply two numbers', function() {
+    calculator.previousTotal = 3;
+    calculator.multiply(5);
+    const result = calculator.runningTotal
+    assert.equal(result, 15)
+  })
+
+  it('it can divide two numbers', function() {
+    calculator.previousTotal = 21;
+    calculator.divide(7);
+    const result = calculator.runningTotal
+    assert.equal(result, 3)
+  })
+
 });
